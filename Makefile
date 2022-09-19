ebc9888966bb09026a5f7931f661ae6801a89e0b
Faun78 <galaxia.andromeda234@gmail.com>
2022-09-19 14:31:32 +0200

    betterpickone

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 a:
 	g++ -c main.cpp -I ./include
-	g++ main.o -o app -L./include -lsfml-graphics-2 -lsfml-window-2 -lsfml-system-2
-	app.exe <zadani1.txt >reseni.txt
-	app.exe >zadani1.txt <reseni.txt
+	g++ main.o -o app -L./ -lsfml-graphics-2 -lsfml-window-2 -lsfml-system-2
+	./app <zadani.txt >reseni.txt
+	./app >zadani1.txt <reseni.txt
 	make
-
-	
--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ mt19937 mt8(time(0));
 sf::Vector2f start(75-5,350-5);
 sf::Vector2f end11(350-5,75-5);
 int popu=110;
-bool newgen=false;
+bool newgen=true;
 double numberofpart=35;
 vector<car> alive;
 int generation=0;
@@ -27,23 +27,24 @@ void newGeneration(NeuralNetwork BB1){
     alive.clear();
     alive.resize(popu);
     if(newgen){
-    for(int iiii=0;iiii<popu;iiii++){
-        alive[iiii].pp=Particle(start.x+4,start.y+4,numberofpart);
-        alive[iiii].score=0;
-        alive[iiii].finished=false;
-        newgen=false;
+        for(int iiii=0;iiii<popu;iiii++){
+            alive[iiii].pp=Particle(start.x+4,start.y+4,numberofpart);
+            alive[iiii].score=0;
+            alive[iiii].finished=false;
+            newgen=false;
     }
     }else{
         for(int iiii=0;iiii<popu;iiii++){
-        alive[iiii].pp=Particle(start.x+4,start.y+4,numberofpart);
+            alive[iiii].pp=Particle(start.x+4,start.y+4,numberofpart);
         if(iiii!=0){
-        alive[iiii].pp.brain=NeuralNetwork::mutate4(BB1,0.1+iiii/100);
+            alive[iiii].pp.brain=NeuralNetwork::mutate4(BB1,pow(0.1+iiii/popu,2));
         }else{
            alive[iiii].pp.brain=BB1; 
         }
         alive[iiii].score=0;
         alive[iiii].finished=false;
-    }}
+        }
+    }
 }
 
 vector<Vector2f> outside;
@@ -62,7 +63,7 @@ void regeneratewalls(){
     double seed =dis6(mt2);
     outside.clear();
     inside.clear();
-     static const double TWOPI = 6.2831853071795865;
+    static const double TWOPI = 6.2831853071795865;
     for(double a=0;a<TWOPI;a+=toradian(25)){
         double xoff=mapp5(cos(a),-1,1,0,noisemax);
         double yoff=mapp5(cos(a),-1,1,0,noisemax);
@@ -89,10 +90,10 @@ void regeneratewalls(){
     checkpoint.clear();
     for(int i=0;i<outside.size();i++){
         if(i==outside.size()-1){
-        walls.push_back(Boundry(outside[i].x,outside[i].y,outside[0].x,outside[0].y,0.5));
-        walls.push_back(Boundry(inside[i].x,inside[i].y,inside[0].x,inside[0].y,0.5));
-        walls.push_back(Boundry(outside[0].x-10,outside[0].y-20,inside[0].x+10,inside[0].y-20,0.5));
-        checkpoint.push_back(Boundry(outside[i].x,outside[i].y,inside[i].x,inside[i].y,0.5));        
+            walls.push_back(Boundry(outside[i].x,outside[i].y,outside[0].x,outside[0].y,0.5));
+            walls.push_back(Boundry(inside[i].x,inside[i].y,inside[0].x,inside[0].y,0.5));
+            walls.push_back(Boundry(outside[0].x-10,outside[0].y-20,inside[0].x+10,inside[0].y-20,0.5));
+            checkpoint.push_back(Boundry(outside[i].x,outside[i].y,inside[i].x,inside[i].y,0.5));        
             break;
         }
         walls.push_back(Boundry(outside[i].x,outside[i].y,outside[i+1].x,outside[i+1].y,0.5));
@@ -104,13 +105,11 @@ void regeneratewalls(){
     end11.x=check11[check11.size()-1].x-5;
     end11.y=check11[check11.size()-1].y-5;
     check11.clear();
-
 }
 int main(){
     l1=lcg();
     l1.setSeed11(0);
     cerr<<l1.m11<<endl;
-    //l1.seed11=abs(mt5()*l1.m11);
     cerr<<l1.seed11<<endl;
     double xoff=0;
     double yoff=0;
@@ -128,8 +127,6 @@ int main(){
     end1.setPosition(end11);
     int numofwall=8;
     //walls
-        
-
         /*{
         walls.push_back(Boundry(50,400,50,200,0.5));
         walls.push_back(Boundry(50,200,150,50,0.5));
@@ -143,12 +140,9 @@ int main(){
         walls.push_back(Boundry(0,0,400,0,0));
     checkpoint.push_back(Boundry(350,50,350,100,0.5));
         }*/
-        
     //walls
     vector<car> dead;
     long long cycle=0;
-
-    // cerr<<"part done\n";
     window.setFramerateLimit(60);
     while (window.isOpen()){
         cycle++;
@@ -156,76 +150,57 @@ int main(){
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed){
                 int maxiu1=0;
-            // cerr<<"closing1"<<endl;
-            for(int uu=0;uu<dead.size();uu++){
-                if(dead[uu].score>dead[maxiu1].score){
-                    maxiu1=uu;
+                for(int uu=0;uu<dead.size();uu++){
+                    if(dead[uu].score>dead[maxiu1].score){
+                        maxiu1=uu;
+                    }
                 }
-                }
-    //pick one function
-        
-        
-        dead[maxiu1].pp.brain.exportBrain();
-        //newGeneration(dead[maxiu].pp.brain);
-        cerr<<dead[maxiu1].score<<endl;
-        
-        //window.close();
-        //dead.clear();
-            cerr<<"closing\n";
+                //pick one function
+                dead[maxiu1].pp.brain.exportBrain();
+                //newGeneration(dead[maxiu].pp.brain);
+                cerr<<dead[maxiu1].score<<endl;
+                cerr<<"closing\n";
                 window.close();
-                }
+            }
         }
         window.clear();
 
         //generate new track
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::F5)){
-            /*walls.clear();
-            for(int jjj=0;jjj<numofwall;jjj++){
-        walls.push_back(Boundry(mt8()%int(width),mt8()%int(width),mt8()%int(height),mt8()%int(height)));
-        }*/
-        regeneratewalls();
-        sta1.setPosition(start);
-        end1.setPosition(end11);
+            regeneratewalls();
+            sta1.setPosition(start);
+            end1.setPosition(end11);
 
         }
         //generate new track
         //showing the cars and applying forces
         for(int i=0;i<alive.size();i++){
-        alive[i].pp.applyforce(sf::Vector2f(dis8(mt2),dis8(mt2)));        
-        alive[i].pp.update();
-        alive[i].score+=1;
-            // cerr<<"checking\n";
-       
-        alive[i].finished=alive[i].pp.check();
-        
-            // cerr<<"checked\n";
-        
-        bool aa=alive[i].pp.show(walls);
-        if(aa||alive[i].finished){
-            //alive[i].score+=accumulate(alive[i].pp.checks.begin(),alive[i].pp.checks.end(),0)*1000;
-            dead.resize(dead.size()+1);
-            swap(alive[i],alive[alive.size()-1]);
-            swap(dead[dead.size()-1],alive[alive.size()-1]);
-            alive.pop_back();
-            i-=1;
+            alive[i].pp.applyforce(sf::Vector2f(dis8(mt2),dis8(mt2)));        
+            alive[i].pp.update();
+            alive[i].score+=1;
+            alive[i].finished=alive[i].pp.check();
+            bool aa=alive[i].pp.show(walls);
+            if(aa||alive[i].finished){
+                //alive[i].score+=accumulate(alive[i].pp.checks.begin(),alive[i].pp.checks.end(),0)*1000;
+                int numofcehckspass =accumulate(alive[i].pp.checks.begin(),alive[i].pp.checks.end(),0);
+
+                alive[i].score=(3000-alive[i].score)*(numofcehckspass*2);
+                dead.resize(dead.size()+1);
+                swap(alive[i],alive[alive.size()-1]);
+                swap(dead[dead.size()-1],alive[alive.size()-1]);
+                alive.pop_back();
+                i-=1;
+            }
         }
-        }
-        //showing the cars and applying forces
-            //drawing start and end
-             window.draw(sta1);
-             window.draw(end1);
-            //drawing start and end
-        //drawing walls and checkpoints
-        
+        window.draw(sta1);
+        window.draw(end1);
         for(auto wall:walls){
             wall.draw();
         }
         for(auto check:checkpoint){
             check.draw();
         }
-        //drawing walls and checkpoints
     window.display();
-    //pick one function
     if(dead.size()==popu &&cycle%100==0||cycle>min(max(200*generation,1500),3000)||sf::Keyboard::isKeyPressed(sf::Keyboard::F7)&&cycle%100==0){
         cerr<<"cleaning\n";
         for(int i=0;i<alive.size();i++){
@@ -240,33 +215,18 @@ int main(){
         int maxiub=0;
         int maxchecks=0;
         for(int uu=0;uu<dead.size();uu++){
-            int numofcehckspass =accumulate(dead[uu].pp.checks.begin(),dead[uu].pp.checks.end(),0);
-            //cerr<<numofcehckspass<<endl;
-        if(numofcehckspass>=maxchecks){
-            //cerr<<numofcehckspass<<endl;
-            if(numofcehckspass==maxchecks){
-                if(dead[uu].score>dead[maxiu].score){
-                    continue;
-                }
-            }
+            if(dead[uu].score>dead[maxiu].score){
                 maxiu=uu;
-                maxchecks=numofcehckspass;
-            }
-            if(dead[uu].finished){
-                if(dead[uu].score<dead[maxiub].score||!dead[maxiub].finished)
-                maxiub=uu;
             }
         }
     //pick one function
         cerr<<maxiub<<endl;
         if(maxiub!=0){
-        newGeneration(dead[maxiub].pp.brain);
-        //dead[maxiub].pp.brain.exportBrain();
-        cerr<<dead[maxiub].score<<" finished "<<maxiub<<endl;
+            newGeneration(dead[maxiub].pp.brain);
+            cerr<<dead[maxiub].score<<" finished "<<maxiub<<endl;
         }else{
-        //dead[maxiu].pp.brain.exportBrain();
-        cerr<<dead[maxiu].score<<" "<<maxchecks<<" "<<dead.size()<<" "<<alive.size()<<endl;
-        newGeneration(dead[maxiu].pp.brain);
+            cerr<<dead[maxiu].score<<" "<<maxchecks<<" "<<dead.size()<<" "<<alive.size()<<endl;
+            newGeneration(dead[maxiu].pp.brain);
         }
         //window.close();
         dead.clear();
